6e88646735aa25935aea708862d31e4edc4f8a10
Daniel Rocco <drocco@gmail.com>
2016-02-09 22:55:15 -0500

    Strip demo down to essentials

--- pig/tools/monodemo.py
+++ pig/tools/monodemo.py
@@ -8,7 +8,6 @@ Demonstration of the workflow and commands PiG uses to geotag an image.
 
 from collections import deque
 from threading import Thread
-from time import sleep, time
 
 from gi.repository import GExiv2
 from gps import gps, isotime, WATCH_ENABLE
@@ -23,21 +22,6 @@ geothread = None
 def read_gps():
     global last_fix
 
-    # p = Popen(
-    #         'gpsbabel -T -i garmin -f usb: -o tabsep -F -'.split(' '),
-    #         stdout=PIPE,
-    #     )
-
-    # try:
-    #     while running:
-    #         line = p.stdout.readline().split('\t')
-    #         line = [line[7], line[8], line[18], line[20]]
-    #         print line
-    #         last_fix.append(line)
-    # finally:
-    #     print 'NOT QUITE DEAD YET!'
-    #     p.kill()
-
     session = gps(mode=WATCH_ENABLE)
 
     while running:
@@ -45,7 +29,6 @@ def read_gps():
 
         if fix['class'] == 'TPV':
             fix = [fix.lon, fix.lat, fix.alt, isotime(fix.time)]
-            # print fix
             last_fix.append(fix)
 
 
@@ -54,20 +37,6 @@ def init():
 
     geothread = Thread(target=read_gps)
     geothread.start()
-
-    sleep(2)
-
-    init_camera()
-
-
-def init_camera():
-    camera_time = check_output('gphoto2 --get-config /main/settings/datetime'.split(' '))
-    systime = check_output(['date', '+%s'])
-
-    print 'Camera time:', camera_time.strip()
-    print 'Python: ', time()
-    print 'System: ', systime.strip()
-    print 'GPS:    ', last_fix[0][-1]
 
 
 def capture():
@@ -105,5 +74,3 @@ def main():
 
     running = False
     geothread.join()
-
-    print last_fix
